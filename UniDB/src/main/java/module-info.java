8041b94d245cdbc616a5b5f000da43f55bbc85d1
AmirHossien Karimi <amirhkz1384@gmail.com>
2025-12-03 22:15:30 +0330

    initialize storage collections and add .gitignore

---
+++ UniDB/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.unidb {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.unidb to javafx.fxml;
+    exports com.example.unidb;
+}
