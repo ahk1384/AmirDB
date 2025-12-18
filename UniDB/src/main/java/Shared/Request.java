bea0717f5b6a5f41f14c92da8ab700343e52296c
AmirHossien Karimi <amirhkz1384@gmail.com>
2025-12-18 12:29:33 +0330

    implement Serializable interface in Request and Response classes; add constructors and getter methods for message handling

--- UniDB/src/main/java/Shared/Request.java
+++ UniDB/src/main/java/Shared/Request.java
@@ -1,4 +1,18 @@
 package Shared;
 
-public class Request {
+import java.io.Serializable;
+
+public class Request implements Serializable {
+    public MessageType messageType;
+    public String query;
+    public Request (MessageType messageType, String query) {
+        this.messageType = messageType;
+        this.query = query;
+    }
+    public MessageType getMessageType() {
+        return messageType;
+    }
+    public String getquery() {
+        return query;
+    }
 }
--- UniDB/src/main/java/Shared/Response.java
+++ UniDB/src/main/java/Shared/Response.java
@@ -1,4 +1,22 @@
 package Shared;
 
-public class Response {
+import java.io.Serializable;
+
+public class Response implements Serializable {
+    public boolean status;
+    public String message;
+
+    public Response(boolean status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
 }
